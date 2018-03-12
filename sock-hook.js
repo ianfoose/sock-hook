d6d34ed2eebb16fb0a4609bfd4b549859b2e0fd3
Ian Foose <ianfoose@me.com>
2018-03-11 23:32:05 -0400

    Update sock-hook.js

--- sock-hook.js
+++ sock-hook.js
@@ -4,33 +4,8 @@ Sock-Hook
 
 A webhook for a socket based broadcasting server
 
-Copyright 2017 Ian Foose
+Copyright 2018 Ian Foose
 */
-
-var apiServer;
-var socketServer;
-
-// Express
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var router = express.Router();
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// gets the router instance
-module.exports.getRouter = function() {
-	return router;
-}
-
-// gets the socket server instance
-module.exports.getSocketServer = function() {
-	return socketServer;
-}
-
-// gets the express server instance
-module.exports.getExpressServer = function() {
-	return apiServer;
-}
 
 // creates the api and socket server(s)
 module.exports.createServer = function(apiOptions, socketOptions) {
@@ -41,6 +16,16 @@ module.exports.createServer = function(apiOptions, socketOptions) {
 	var apiPort = 80;
 	var socketPort = 8080;
 	var socketSSL;
+
+	var apiServer;
+	var socketServer;
+
+	// Express
+	var express = require('express');
+	var app = express();
+	var bodyParser = require('body-parser');
+	var router = express.Router();
+	app.use(bodyParser.urlencoded({ extended: true }));
 
 	if(apiOptions) {
 		if(apiOptions.port && !isNaN(apiOptions.port)) {
@@ -77,16 +62,7 @@ module.exports.createServer = function(apiOptions, socketOptions) {
 
 	socketServer = wicker.createSocketServer(socketConfig);
 
-	// run server
-
-	router.post('/sendData', function(req, res) {
-		if(req.body.data) {
-			wicker.sendToAll(req.body.data,'utf8');
-			res.sendStatus(200);
-		} else {
-			res.sendStatus(400);
-		}
-	});
-
 	app.use('/',router);
+
+	return { socketServer: { wicker:wicker, server:socketServer }, router: router };
 }
